679c3c2318741371868e35a76026b17c1f76c957
yangsimple <yangyi981121@gmail.com>
2021-05-31 00:59:25 +0800

    Revert "测试"
    This reverts commit 8d481e423be22840c77716a756180459c8463c82.

--- components/Author.js
+++ components/Author.js
@@ -8,7 +8,7 @@ const Author =()=>{
         <div className="author-div comm-box">
             <div> <Avatar size={100} src="https://ftp.bmp.ovh/imgs/2021/03/d0404e15a98e7e6e.jpg"  /></div>
             <div className="author-introduction">
-                23333
+                222222222222
                 <Divider>社交账号</Divider>
                 <Avatar size={28}  className="account"  />
                 <Avatar size={28}   className="account" />
