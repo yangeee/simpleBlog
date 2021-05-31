80176313c91ef65d5db60bf84b5457ae0afc6bfc
yangeee <534650291@qq.com>
2021-05-31 10:34:38 +0800

    【更新】：添加备案号

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,10 @@ const Footer = ()=>(
     <div className="footer-div">
         <div>系统由 React+Node+Ant Design驱动 </div>
         <div>simple.com</div>
+        <div>
+            ICP证：<a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2021071273号</a>
+        </div>
+
     </div>
 )
 
