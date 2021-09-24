e5eb52b7d687af9726b35884e4af12c740a89851
yangeee <534650291@qq.com>
2021-09-24 11:52:34 +0800

    【更新】：升级next版本试下能否展示图片

--- components/Advert.js
+++ components/Advert.js
@@ -3,10 +3,6 @@ import '@sc/advert.module.css'
 const Advert = ()=>{
     return (
         <div className="ad-div comm-box">
-            <div><img src="https://ftp.bmp.ovh/imgs/2021/03/ed2fbf60caf12bdf.jpg" width="100%" /></div>
-            <div><img src="https://ftp.bmp.ovh/imgs/2021/03/c3abc0891872b6ee.png" width="100%" /></div>
-            <div><img src="https://ftp.bmp.ovh/imgs/2021/03/262efd55648a6ff9.jpg" width="100%" /></div>
-            <div><img src="https://ftp.bmp.ovh/imgs/2021/03/34ef35140b71f128.jpg" width="100%" /></div>
         </div>
     )
 }
